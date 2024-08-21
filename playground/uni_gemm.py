d17157edc6872c9704ed758e10a8d1a2ade62217
adrianliu <adrianliu00@gmail.com>
2024-08-20 21:24:40 -0400

    schedule update

--- playground/uni_gemm.py
+++ playground/uni_gemm.py
@@ -7,6 +7,7 @@ import allo
 from allo.ir.types import int8, int16, int32, int64, int128, int256, int512, bool
 from allo.utils import get_np_struct_type
 import allo.backend.hls as hls
+from allo.ir.utils import MockBuffer
 
 
 from datetime import datetime
@@ -31,25 +32,60 @@ def test_PE_simple():
     print(s_top.module)
 
 
+def test_tile():
+    from allo.library.systolic_uni import systolic_tile_uni
 
-def test_gemm_simple():
+    # =================================================================
+
+    # M0 = 128
+    # K0 = 768
+    # N0 = 3072
+    # Rt0 = 16
+    # Ct0 = 16
+
+    M0 = 16
+    K0 = 32
+    N0 = 16
+    Rt0 = 4
+    Ct0 = 4
+
+    np_type = np.int8
+    allo_type = int8
+
+    # =================================================================
+
+    s_tile = allo.customize(systolic_tile_uni, instantiate=[int8, Rt0, Ct0])
+
+    tile_name = "systolic_tile_uni"
+    pe = s_tile.unfold(f"{tile_name}:PE", [0, 1])
+    # s_top.to(MockBuffer(systolic_name, "R_buf"), pe, axis=0, depth=Ct0 + 1)
+    # s_top.to(MockBuffer(systolic_name, "C_buf"), pe, axis=1, depth=Rt0 + 1)
+
+    code = s_tile.build(target="vhls")
+    with open(f'tileHLS_{date}.cpp', 'w') as f:
+        print(code, file=f)
+    
+
+
+
+def test_gemm():
     from allo.library.systolic import systolic
     from allo.library.systolic_refine import systolic_ws, systolic_os
     from allo.library.systolic_uni import systolic_uni
 
     # =================================================================
 
-    M0 = 128
-    K0 = 768
-    N0 = 3072
-    Rt0 = 16
-    Ct0 = 16
+    # M0 = 128
+    # K0 = 768
+    # N0 = 3072
+    # Rt0 = 16
+    # Ct0 = 16
 
-    # M0 = 8
-    # K0 = 2
-    # N0 = 4
-    # Rt0 = 2
-    # Ct0 = 4
+    M0 = 16
+    K0 = 32
+    N0 = 16
+    Rt0 = 4
+    Ct0 = 4
 
     np_type = np.int8
     allo_type = int8
@@ -75,7 +111,10 @@ def test_gemm_simple():
         # systolic_os[int8, int8, int8, M0, K0, N0, Rt0, Ct0](X, W_A, Z)
         return Z
     
+    # s_uni = allo.customize(systolic_uni, instantiate=[int8, M0, K0, N0, Rt0, Ct0])
+    # s_uni.unfold()
     s_top = allo.customize(top, instantiate=[allo_type])
+    # s_top.compose(s_uni)
 
     # if Rt0 < 20:
     #     with open(f'systolic_{date}.mlir', 'w') as f:
@@ -101,16 +140,27 @@ def test_gemm_simple():
     # ---------------------------------------
     # Scheduling
     tile_name = "systolic_tile_uni"
+    systolic_name = "systolic_uni"
+    # s_top.compose(systolic_uni, instantiate=[int8, M0, K0, N0, Rt0, Ct0])
+    # s_top.partition(s_top.R_buf, dim=1)
+
+    pe = s_top.unfold(f"{tile_name}:PE", [0, 1])
+    s_top.to(MockBuffer(systolic_name, "R_buf"), pe, axis=0, depth=Ct0 + 1)
+    s_top.to(MockBuffer(systolic_name, "C_buf"), pe, axis=1, depth=Rt0 + 1)
+
+    # s_top.to(MockBuffer(tile_name, "R_buf"), pe, axis=0, depth=Ct0 + 1)
+    # s_top.to(MockBuffer(tile_name, "C_buf"), pe, axis=1, depth=Rt0 + 1)
+
 
     # ---------------------------------------
-    # code = s_top.build(target="vhls")
-    # if Rt0 < 20:
-    #     with open(f'systolicHLS_{date}.cpp', 'w') as f:
-    #         print(code, file=f)
+    code = s_top.build(target="vhls")
+    if Rt0 < 20:
+        with open(f'systolicHLS_{date}.cpp', 'w') as f:
+            print(code, file=f)
 
     # ---------------------------------------
-    mod_v = s_top.build(target="vhls", mode='csyn', project=f"gemm_{date}.prj")
-    mod_v()
+    # mod_v = s_top.build(target="vhls", mode='csyn', project=f"gemm_{date}.prj")
+    # mod_v()
 
     # s_top.compose(
     #     systolic, instantiate=[int32, int32, int32, L, D, 4 * D, M0, M1]
@@ -153,5 +203,5 @@ def test_gemm_simple():
 
 if __name__ == '__main__':
     # test_PE_simple()
-    
-    test_gemm_simple()
+    test_tile()
+    # test_gemm()
